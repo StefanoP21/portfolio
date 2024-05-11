243b180c7aa37ae09d8b4b8b9b2963a2b7e22ad0
Stefano <stefanop21@outlook.es>
2024-05-11 16:29:18 -0500

    update: skills and projects data

--- lib/data.ts
+++ lib/data.ts
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   FaReact,
-  FaJava,
   FaBootstrap,
   FaCode,
   FaTrello,
@@ -16,18 +15,13 @@ import {
 } from 'react-icons/bi';
 import {
   SiNextdotjs,
-  SiPhp,
-  SiSpring,
   SiTailwindcss,
-  SiChakraui,
-  SiMui,
   SiJest,
   SiTestinglibrary,
   SiVisualstudiocode,
   SiPostman,
   SiGnubash,
   SiWebstorm,
-  SiIntellijidea,
   SiFirebase,
   SiRedux,
   SiReactrouter,
@@ -35,7 +29,7 @@ import {
   SiNestjs,
   SiMongodb,
 } from 'react-icons/si';
-import { GrMysql, GrOracle } from 'react-icons/gr';
+import { GrOracle } from 'react-icons/gr';
 import { FaGitAlt } from 'react-icons/fa6';
 import { GoCopilot } from 'react-icons/go';
 import { BsFiletypeSql } from 'react-icons/bs';
@@ -44,9 +38,6 @@ import calendar from '@/public/calendar.png';
 import journal from '@/public/journal.png';
 import tarotspa from '@/public/tarotspa.png';
 import apuchay from '@/public/apuchay.png';
-import giphy from '@/public/giphy.png';
-import teambuilder from '@/public/teambuilder.png';
-import smartbank from '@/public/smartbank.png';
 import coffehunter from '@/public/coffehunter.png';
 import growth from '@/public/growth.png';
 import doguitoform from '@/public/doguitoform.png';
@@ -95,7 +86,12 @@ export const skillsData = [
     color: '#0ea5e9',
   },
   {
-    name: 'Redux Toolkit',
+    name: 'React Router',
+    icon: React.createElement(SiReactrouter),
+    color: '#ef4444',
+  },
+  {
+    name: 'Redux',
     icon: React.createElement(SiRedux),
     color: '#a78bfa',
   },
@@ -128,11 +124,6 @@ export const skillsData = [
     name: 'Firebase',
     icon: React.createElement(SiFirebase),
     color: '#fbbf24',
-  },
-  {
-    name: 'Bootstrap',
-    icon: React.createElement(FaBootstrap),
-    color: '#9333ea',
   },
   {
     name: 'Tailwind CSS',
@@ -222,10 +213,39 @@ export const experiencesData = [
 
 export const projectsData = [
   {
+    title: 'Apuchay',
+    description:
+      'Aplicación web para mejorar el control de las obras públicas. La aplicación cuenta con un sistema de autenticación de usuarios, protección de rutas y cursos de capacitación online.',
+    tags: [
+      'TypeScript',
+      'React',
+      'React Router',
+      'TanStack Query',
+      'Chakra UI',
+      'Node.js',
+      'Express',
+      'MongoDB',
+      'JWT',
+    ],
+    imageUrl: apuchay,
+    demoUrl: 'https://apuchay.vercel.app/',
+    githubUrl: 'https://github.com/StefanoP21/apuchay',
+  },
+  {
     title: 'Calendar App',
     description:
-      'Aplicación web para gestionar eventos personales. La aplicación cuenta con un sistema de autenticación de usuario y protección de rutas. Además, cuenta con un sistema de creación, actualización y eliminación de eventos.',
-    tags: ['React', 'React-router', 'Redux', 'Node.js', 'Express', 'MongoDB'],
+      'Aplicación web para gestionar eventos. La aplicación cuenta con un sistema de autenticación de usuarios, protección de rutas y operaciones CRUD para los eventos.',
+    tags: [
+      'JavaScript',
+      'React',
+      'React Router',
+      'Redux Toolkit',
+      'Chakra UI',
+      'Node.js',
+      'Express',
+      'MongoDB',
+      'JWT',
+    ],
     imageUrl: calendar,
     demoUrl: 'https://calendar-app-v1.vercel.app/',
     githubUrl: 'https://github.com/StefanoP21/calendar-app',
@@ -233,8 +253,15 @@ export const projectsData = [
   {
     title: 'Journal App',
     description:
-      'Aplicación web para escribir notas personales. La aplicación cuenta con un sistema de autenticación de usuario y protección de rutas. Además, cuenta con un sistema de creación, edición y eliminación de notas.',
-    tags: ['React', 'React-router', 'Redux', 'Material UI', 'Firebase'],
+      'Aplicación web para escribir notas personales. La aplicación cuenta con un sistema de autenticación de usuarios, protección de rutas y operaciones CRUD para las notas.',
+    tags: [
+      'JavaScript',
+      'React',
+      'React Router',
+      'Redux Toolkit',
+      'Material UI',
+      'Firebase',
+    ],
     imageUrl: journal,
     demoUrl: 'https://journal-v1.vercel.app/',
     githubUrl: 'https://github.com/StefanoP21/journal-app',
@@ -242,52 +269,16 @@ export const projectsData = [
   {
     title: 'Tarot Cards',
     description:
-      'Aplicación web para leer cartas de tarot con temática CyberPunk 2077. La aplicación cuenta con un sistema de lectura y búsqueda de cartas y un sistema de interpretación de resultados. Además, cuenta con un sistema de protección de rutas.',
-    tags: ['JavaScript', 'React', 'React-router', 'Tailwind CSS'],
+      'Aplicación web para leer cartas de tarot con temática CyberPunk 2077. La aplicación cuenta con un sistema de lectura y búsqueda de cartas. Además, cuenta con protección de rutas.',
+    tags: ['JavaScript', 'React', 'React Router', 'Tailwind CSS'],
     imageUrl: tarotspa,
     demoUrl: 'https://tarot-spa.vercel.app/',
     githubUrl: 'https://github.com/StefanoP21/tarot-spa',
   },
   {
-    title: 'Apuchay',
-    description:
-      'Aplicación web para mejorar el control de las obras públicas. La aplicación cuenta con una interfaz orientada al ciudadano común. Además, cuenta con un sistema de reportes de obras y cursos de capacitación online.',
-    tags: ['JavaScript', 'React', 'React-router', 'Chakra UI'],
-    imageUrl: apuchay,
-    demoUrl: 'https://stefanop21.github.io/hackaton-cgr2023/',
-    githubUrl: 'https://github.com/StefanoP21/hackaton-cgr2023',
-  },
-  {
-    title: 'Organizador de Equipos',
-    description:
-      'Aplicación web para ayudar en la organización de equipos de trabajo. La aplicación permite crear plantillas de personal y asignarlos a un equipo. Etiqueta miembros favoritos y eliminalos.',
-    tags: ['JavaScript', 'React', 'Styled-components'],
-    imageUrl: teambuilder,
-    demoUrl: 'https://registro-personal-org.vercel.app/',
-    githubUrl: 'https://github.com/StefanoP21/registro-personal-org',
-  },
-  {
-    title: 'Giphy',
-    description:
-      'Aplicación web para ver GIFs. La aplicación cuenta con un buscador de GIFs y utiliza la API de GIPHY Developers para mostrar los resultados.',
-    tags: ['JavaScript', 'React', 'Styled-components'],
-    imageUrl: giphy,
-    demoUrl: 'https://stefanop21.github.io/giphy-app/',
-    githubUrl: 'https://github.com/StefanoP21/giphy-app',
-  },
-  {
-    title: 'Smart Bank',
-    description:
-      'Aplicación web para la gestión de finanzas personales. La aplicación simula un estado de cuenta bancario y cuenta con funcionalidades para ocultar y mostrar el saldo, así como cambiar el tema.',
-    tags: ['JavaScript', 'React', 'Styled-components'],
-    imageUrl: smartbank,
-    demoUrl: 'https://smart-bank-liard.vercel.app/',
-    githubUrl: 'https://github.com/StefanoP21/smart-bank',
-  },
-  {
     title: 'CoffeHunter',
     description:
-      'Aplicación web para una marca de café peruana. La aplicación web cuenta con un catálogo de productos, una galería de fotos, un formulario de contacto y un CRUD para administrar los productos. ',
+      'Aplicación web para una marca de café peruana. La aplicación web cuenta con un catálogo de productos, una galería de fotos, un formulario de contacto y operaciones CRUD para administrar los productos.',
     tags: ['HTML', 'CSS', 'JavaScript', 'Bootstrap', 'PHP', 'MySQL'],
     imageUrl: coffehunter,
     demoUrl: 'https://stefanop21.000webhostapp.com/',
@@ -296,34 +287,16 @@ export const projectsData = [
   {
     title: 'Growth App',
     description:
-      'Landing page para una startup de dashboards online. La aplicación cuenta con una interfaz moderna y un diseño responsivo. Además, cuenta con un múltiples efectos de interacción con el usuario.',
+      'Landing page para una startup de dashboards online. La aplicación cuenta con una interfaz moderna y un diseño responsivo. Además, cuenta con un múltiples efectos de interacción para el usuario.',
     tags: ['HTML', 'CSS', 'JavaScript'],
     imageUrl: growth,
     demoUrl: 'https://stefanop21.github.io/saas-website/',
     githubUrl: 'https://github.com/StefanoP21/saas-website',
   },
-  // {
-  //   title: 'Pokédex',
-  //   description:
-  //     'Aplicación web para ver información de los pokémon de la primera generación. La aplicación utiliza la API de PokéAPI para mostrar los resultados. Además, cuenta con un listado de filtros para mostrar pokémon por tipo.',
-  //   tags: ['HTML', 'CSS', 'JavaScript'],
-  //   imageUrl: pokedex,
-  //   demoUrl: 'https://stefanop21.github.io/pokedex/',
-  //   githubUrl: 'https://github.com/StefanoP21/pokedex',
-  // },
-  // {
-  //   title: 'TODO App',
-  //   description:
-  //     'Aplicación web para gestionar tareas. La aplicación permite crear y eliminar tareas. Además, cuenta con un sistema de etiquetas para clasificar las tareas cumplidas.',
-  //   tags: ['HTML', 'CSS', 'JavaScript'],
-  //   imageUrl: todoapp,
-  //   demoUrl: 'https://stefanop21.github.io/todo-app/',
-  //   githubUrl: 'https://github.com/StefanoP21/todo-app',
-  // },
   {
     title: 'PetShop',
     description:
-      'Aplicación web para la gestión de formularios de una tienda de mascotas. La aplicación cuenta con un formulario de registro. Además, cuenta con un sistema de validación de campos y un sistema de alertas.',
+      'Aplicación web para la gestión de formularios de una tienda de mascotas. La aplicación cuenta con un formulario de registro. Además, cuenta con un sistema de validación de campos y alertas de errores.',
     tags: ['HTML', 'CSS', 'JavaScript'],
     imageUrl: doguitoform,
     demoUrl: 'https://stefanop21.github.io/formulario-petshop/',
